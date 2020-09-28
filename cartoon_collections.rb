26303e84c4f83c44913838ad9af0bb3a4027d5c8
akam alam <arundelfootball23@gmail.com>
2020-09-28 01:54:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,10 +10,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
-  
+ 
   array.each do | dwarves|
   puts dwarves
 end 
