832b5717b36a8a3be3ebe185c1d09319b698f780
akam alam <arundelfootball23@gmail.com>
2020-09-28 01:52:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,8 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   
-  array.each do |index, dwarves|
-  puts dwarves
+  array.each do | dwarves|
+  puts dwarves.index 
 end 
   
 end
